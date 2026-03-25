65fe520160d4775dfb690f28c0decd61509290f6
Khanh Pham <phamduckhanh2411@gmail.com>
2026-03-25 23:23:59 +0700

    allow all built-in modules by defaults

--- packages/@n8n/task-runner/src/config/js-runner-config.ts
+++ packages/@n8n/task-runner/src/config/js-runner-config.ts
@@ -3,7 +3,7 @@ import { Config, Env } from '@n8n/config';
 @Config
 export class JsRunnerConfig {
 	@Env('NODE_FUNCTION_ALLOW_BUILTIN')
-	allowedBuiltInModules: string = '';
+	allowedBuiltInModules: string = '*';
 
 	@Env('NODE_FUNCTION_ALLOW_EXTERNAL')
 	allowedExternalModules: string = '';
